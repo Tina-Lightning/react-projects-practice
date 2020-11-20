8f4d085176a79139321ace8520dcc0f8459feb9d
Tina-Lightning <c.atherholt@gmail.com>
2020-11-20 09:38:16 -0500

    completed lorem ipsum generator

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,24 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  }
+  return (
+    <>
+    <section className="container">
+    <h3>color generator</h3>
+    </section>
+    <section className="colors">
+      <h4>list goes here</h4>
+    </section>
+    </>
+  )
 }
 
 export default App
