62711eb919e3e81b5dde666e6670cf9ab7465f34
Tina-Lightning <c.atherholt@gmail.com>
2020-11-23 11:11:28 -0500

    color generator project

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -10,16 +10,34 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    console.log(colors)
+    } catch (error) {
+      setError(true);
+      console.log(error)
+    }
   }
+
   return (
     <>
     <section className="container">
     <h3>color generator</h3>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025" className={`${error ? "error" : null}`} />
+      <button className="btn" type="submit">
+        submit
+      </button>
+    </form>
     </section>
     <section className="colors">
-      <h4>list goes here</h4>
-    </section>
+      {list.map((color, index) => {
+        const hex = color.hex
+        console.log(color)
+        return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
+      })}
+    </section> 
     </>
   )
 }
--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index, hexColor}) => {
+  //console.log(hexColor)
+  const [alert, setAlert] = useState(false);
+  const background = rgb.join(",")
+  const hex = rgbToHex(...rgb);
+
+  console.log(background)
+  return <article className={`color`} style={{backgroundColor:`rgb(${background})`}}>
+    <p className="percent-value">
+      {weight}%
+    </p>
+    <p className="color-value">{hexColor}</p>
+  </article>
 }
 
 export default SingleColor
