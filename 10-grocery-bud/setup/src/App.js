9bfb0b59802ecf08f3321e61772b1900667e167b
Tina-Lightning <c.atherholt@gmail.com>
2020-11-25 10:33:26 -0500

    worked on grocery list

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -7,31 +7,59 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" })
+  const [alert, setAlert] = useState({ 
+    show: false, 
+    msg: "", 
+    type: "" 
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    if (!name) {
+      // display alert
+      showAlert(true, "danger", "please enter value")
+    }
+    else if (name && isEditing) {
+      // deal with edit
+    }
+    else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem])
+      setName("")
+    }
+  }
+
+  const showAlert = (show=false, type="", msg="") => {
+    setAlert({show, type, msg})
   }
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-          {alert.show && <Alert/>}
-          <h3>grocery buddy</h3>
-          <div className="form-control">
-            <input type="text" />
-            <button type="submit" className="submit-btn">
-              {isEditing ? "edit" : "submit"}
-            </button>
-          </div>
+        {alert.show && <Alert {...alert} />}
+        <h3>grocery buddy</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">
-          clear items
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">
+            clear items
         </button>
-      </div>
+        </div>
+      )}
     </section>
   )
 }
