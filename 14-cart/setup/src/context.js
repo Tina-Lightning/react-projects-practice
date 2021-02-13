59faf7690660f5e336c0c14a897e9293a2386a99
Tina-Lightning <c.atherholt@gmail.com>
2021-02-13 13:55:32 -0500

    finished cart project

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -29,6 +29,25 @@ const AppProvider = ({ children }) => {
     dispatch({ type: "DECREASE", payload: id });
   };
 
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
   return (
     <AppContext.Provider
       value={{
@@ -37,6 +56,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
       }}
     >
       {children}
